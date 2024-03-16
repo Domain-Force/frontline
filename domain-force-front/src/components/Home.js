7b308d2240295213fabee9cefa2416a28648aa27
Resilliance <JKEsguerra2050@yahoo.com>
2024-03-16 11:08:39 -0400

    [ADD][DOM-18] Initialized routes and started UI/UX

---
+++ domain-force-front/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+	<div>
+	  <p className="zero-to-production">zero to production</p>
+	</div>
+  );
+}
+
+export default Home;
---
+++ domain-force-front/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function NavBar() {
+  return (
+	<nav>
+	  <ul className="navbar">
+		<li>
+			<NavLink activeClassName="active" exact to="/">home</NavLink>
+		</li>
+		<li>
+			<NavLink activeClassName="active" to="/courses">courses</NavLink>
+		</li>
+		<li>
+			<NavLink activeClassName="active" to="/about">about</NavLink>	
+		</li>
+	  </ul>
+	</nav>
+  );
+}
+
+export default NavBar;
