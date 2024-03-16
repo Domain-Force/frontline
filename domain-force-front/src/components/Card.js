3c3c0a4736b7a436eb5f60df5dabbba43df1c8e8
Resilliance <JKEsguerra2050@yahoo.com>
2024-03-16 12:17:16 -0400

    [ADD][DOM-18] Finished setting up the courses and articles card text

--- domain-force-front/src/components/Card.js
+++ domain-force-front/src/components/Card.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function Card({title, description, color}) {
+function Card({title, description, descList, color}) {
   return (
 	<div className="card" style={{ backgroundColor: color }}>
 	  <p className="card-title">{title}</p>
 	  <p className="card-description">{description}</p>
+	   {descList && descList.length > 0 && (
+        <ul>
+          {descList.map((desc, index) => (
+            <li key={index}>{desc}</li>
+          ))}
+        </ul>
+		)}
 	</div>
   );
 }
--- domain-force-front/src/components/Home.js
+++ domain-force-front/src/components/Home.js
@@ -18,14 +18,17 @@ function Home() {
 			<Card 
 				title="domain-force speedrun"
 				description="how domain force was made:"
+				descList={['tech stack deliberation', 'UI/UX and database design', 'setting up domains, servers, https','questioning my life choices']}
 	  			color="#FF9C9C"/>
 			<Card 
 				title="django or nothing"
-				description="chad or python"
+				description="how to use & deploy django"
+				descList={['serving HTML templates', 'confiuring routes (urls)', 'why python makes you chad','what is bootstrap and how to use it']}
 	  			color="#A0C8F8"/>
 			<Card 
 				title="third rate developer, fourth rate production"
 				description="an approach to software architecture"
+				descList={["what is a front and back end?", 'SQL vs NoSQL vs Graph', 'to docker or not to docker','why does it matter; literally']}
 	  			color="#9CD000"/>
 		</div>
 
@@ -38,15 +41,18 @@ function Home() {
 	  <div className="cards">
 		<Card
 			title="moby docker (docker)"
-			description="how to make a container"
+			description="how docker? where docker?"
+			descList={['what docker?', 'using docker for your projects', 'iT rUnS oN mY MaChInE','docker vs native']}
 	  		color="#FFD89E"/>
 		<Card
 			title="domain name servers"
 			description="what's a domain? or a name server?"
+			descList={['buying a domain', 'what to avoid', 'suggestions on where to buy','hopefully not getting scammed']}
 	  		color="#E0A773"/>
 		<Card
 			title="cloudflare"
 			description="clouds can have flares?"
+			descList={['what is a domain name server (dns)', 'configuring dns records', 'mom get the camera, my domain is up!']}
 	  		color="#D5BBEA"/>
 	  </div>
 	  </body>
