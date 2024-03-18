11a74cf323300890a6ed3163d1b5bc4a9ec276ef
Resilliance <JKEsguerra2050@yahoo.com>
2024-03-18 09:45:06 -0400

    [ADD][DOM-19] Version 1.0 of the about page

--- domain-force-front/src/components/About.js
+++ domain-force-front/src/components/About.js
@@ -2,8 +2,36 @@ import React from 'react';
 
 function About() {
   return (
-	<div>
-	  <h2>about</h2>
+	<div className="cards-container">
+	  <body>
+		<div>
+			<p className="about-heading">who are we?</p>
+			<div className="about-heading-design" id="ahd1"></div>
+			<div className="about-heading-design" id="ahd2"></div>
+			<div className="about-heading-design" id="ahd3"></div>
+		</div>
+		<p className="about">
+			we are just a bunch of web developers with about 2 years of tenure that want to help people who are starting where we started, from zero.
+		</p>
+		<p className="about">
+			we know how difficult it was for us to get started in software, so we want to do our part for the community and try to supplement the knowledge that is already out there.
+		</p>
+		<p className="about">
+			we try to emphasize the a project based learning methodology instead of a <em><strong>here is the documentation, now go read it</strong></em> approach.
+		</p>
+		<p className="about">
+			structuring our courses in a way that is easy to follow and as straight forward as possible, trying to trim through all the fat possible.
+		</p>
+		<p className="about">
+			we do not want our articles to be a distraction from the course so we try our best to keep them as to the point as possible, and give you the option to learn more if you get interested in a topic.
+		</p>
+		<p className="about">
+			we honestly hope our effort here helps at least one of you, and if it does; we reached our goal. if we managed to help more than just one, that is just our extra credit.
+		</p>
+		<p className="about">
+			happy coding, and keep it fun :) ~ domain force
+		</p>
+	  </body>
 	</div>
   );
 }
