6f4923a481d2d3fa578dd868540870dc7ac0a232
Resilliance <JKEsguerra2050@yahoo.com>
2024-03-20 19:16:03 -0400

    [ADD][DOM-21] Initialized first article list

--- domain-force-front/src/pages/Articles.js
+++ domain-force-front/src/pages/Articles.js
@@ -9,13 +9,20 @@ function Articles() {
 			<div className="article-item-grid">
 				<ArticleItem name="moby docker (docker)" color="#FF9C9C" />
 				<ArticleItem name="django, not the movie" color="#A0C8F8" />
-				<ArticleItem name="learning methodology" color="#A2A0F8" />
-				<ArticleItem name="moby docker (docker)" color="#FF9C9C" />
-				<ArticleItem name="django, not the movie" color="#A0C8F8" />
-				<ArticleItem name="learning methodology" color="#A2A0F8" />
-				<ArticleItem name="moby docker (docker)" color="#FF9C9C" />
-				<ArticleItem name="django, not the movie" color="#A0C8F8" />
-				<ArticleItem name="learning methodology" color="#A2A0F8" />
+				<ArticleItem name="learning methodology" color="#FFD29C" />
+				<ArticleItem name="css (pretty things wow)" color="#A2A0F8" />
+				<ArticleItem name="html (hitmel)" color="#FFAAD3" />
+				<ArticleItem name="javascript (optional ';')" color="#9CD000" />
+				<ArticleItem name="react (js speedrun, any%)" color="#FCEE74" />
+				<ArticleItem name="REST (time to sleep)" color="#96EBF6" />
+				<ArticleItem name="SOAP (time to bathe)" color="#F2AF4A" />
+				<ArticleItem name="jira (not cumin seeds)" color="#96EBF6" />
+				<ArticleItem name="cloudflare (oooh clouds)" color="#C3C9D2" />
+				<ArticleItem name="figma (design cool things)" color="#4489F0" />
+				<ArticleItem name="domains (.com, .co, .org)" color="#A2A0F8" />
+				<ArticleItem name="web hosting" color="#FF9C9C" />
+				<ArticleItem name="postgresql (post squeal)" color="#FCEE74" />
+				<ArticleItem name="python lists (aka arrays)" color="#9CD000" />
 			</div>
 		</div>
 	);
