837ce523cc75fc7b56feac7035b8fd640a9e9e4c
Resilliance <JKEsguerra2050@yahoo.com>
2024-04-20 20:19:37 -0400

    [ADD] Article Item Disabled if not available

--- domain-force-front/src/components/ArticleItem.js
+++ domain-force-front/src/components/ArticleItem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function ArticleItem({ path, name, color }) {
+function ArticleItem({ path, name, color, isComingSoon }) {
   return (
-	<NavLink className="article-item" to={`/articles/${path}`} style={{ backgroundColor: color }}>
-	  {name}
-	</NavLink>
+	  <NavLink 
+      className={`article-item ${isComingSoon ? 'disabled' : ''}`} 
+      to={isComingSoon ? '#' : `/articles/${path}`} 
+      style={{ backgroundColor: color }}
+    >
+      {name}
+      {isComingSoon && (
+        <div className="coming-soon-tooltip">Coming Soon</div>
+      )}
+    </NavLink>
   );
 }
 
--- domain-force-front/src/pages/Articles.js
+++ domain-force-front/src/pages/Articles.js
@@ -8,21 +8,21 @@ function Articles() {
 			<br/>
 			<div className="article-item-grid">
 				<ArticleItem path="docker" name="moby docker (docker)" color="#FF9C9C" />
-				<ArticleItem name="django, not the movie" color="#A0C8F8" />
-				<ArticleItem name="learning methodology" color="#FFD29C" />
-				<ArticleItem name="css (pretty things wow)" color="#A2A0F8" />
-				<ArticleItem name="html (hitmel)" color="#FFAAD3" />
-				<ArticleItem name="javascript (optional ';')" color="#9CD000" />
-				<ArticleItem name="react (js speedrun, any%)" color="#FCEE74" />
-				<ArticleItem name="REST (time to sleep)" color="#96EBF6" />
-				<ArticleItem name="SOAP (time to bathe)" color="#F2AF4A" />
-				<ArticleItem name="jira (not cumin seeds)" color="#96EBF6" />
-				<ArticleItem name="cloudflare (oooh clouds)" color="#C3C9D2" />
-				<ArticleItem name="figma (design cool things)" color="#4489F0" />
+				<ArticleItem name="django, not the movie" color="#A0C8F8" isComingSoon={true}/>
+				<ArticleItem name="learning methodology" color="#FFD29C" isComingSoon={true}/>
+				<ArticleItem name="css (pretty things wow)" color="#A2A0F8" isComingSoon={true}/>
+				<ArticleItem name="html (hitmel)" color="#FFAAD3" isComingSoon={true}/>
+				<ArticleItem name="javascript (optional ';')" color="#9CD000" isComingSoon={true}/>
+				<ArticleItem name="react (js speedrun, any%)" color="#FCEE74" isComingSoon={true}/>
+				<ArticleItem name="REST (time to sleep)" color="#96EBF6" isComingSoon={true}/>
+				<ArticleItem name="SOAP (time to bathe)" color="#F2AF4A" isComingSoon={true}/>
+				<ArticleItem name="jira (not cumin seeds)" color="#96EBF6" isComingSoon={true}/>
+				<ArticleItem name="cloudflare (oooh clouds)" color="#C3C9D2" isComingSoon={true}/>
+				<ArticleItem name="figma (design cool things)" color="#4489F0" isComingSoon={true}/>
 				<ArticleItem path="domains" name="domains (.com, .co, .org)" color="#A2A0F8" />
-				<ArticleItem name="web hosting" color="#FF9C9C" />
-				<ArticleItem name="postgresql (post squeal)" color="#FCEE74" />
-				<ArticleItem name="python lists (aka arrays)" color="#9CD000" />
+				<ArticleItem name="web hosting" color="#FF9C9C" isComingSoon={true}/>
+				<ArticleItem name="postgresql (post squeal)" color="#FCEE74" isComingSoon={true}/>
+				<ArticleItem name="python lists (aka arrays)" color="#9CD000" isComingSoon={true}/>
 			</div>
 		</div>
 	);
